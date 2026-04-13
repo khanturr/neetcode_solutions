8b7ffb707f980a5bf0e0a2b8d72845fea99ca7ab
Hunter Purevbayar <55557543+khanturr@users.noreply.github.com>
2026-04-13 12:56:48 -0400

    Add: top-k-elements-in-list - submission-40

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-40.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #for each element i want to add the frequency as a value, int as key, 
+        #then we sort by frequency, get the top k
+        mymap = {}
+        for n in nums:
+            mymap[n] = 0
+        
+        for n in nums:
+            mymap[n] += 1
+
+        top_keys = sorted(mymap, key=mymap.get, reverse=True)
+        return top_keys[:k]
+        
